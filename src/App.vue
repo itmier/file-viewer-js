1101b7869b83544c0a0d190546cbf0e34d759029
Tmier <1042970366@qq.com>
2023-02-02 14:56:13 +0800

    feat: 示例更新

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 王云飞
  * @Date: 2023-02-01 11:02:45
- * @LastEditTime: 2023-02-02 11:08:29
+ * @LastEditTime: 2023-02-02 14:54:20
  * @LastEditors: 王云飞
  * @Description: 
  * 
@@ -10,11 +10,15 @@
 import '../dist/style.css'
 import FileView from '../dist/file-view.js'
 // import FileView from '../lib/main'
-const fits = ['fill', 'contain', 'cover', 'none', 'scale-down']
-const url =
-  'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
 
-  const toPreview = () => {
+const fits = ['fill', 'contain', 'cover', 'none', 'scale-down']
+const urls = [
+'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg'
+]
+  
+
+  const toPreview = (showUrl) => {
     new FileView({
       files: [
         {
@@ -26,14 +30,14 @@ const url =
           originName: 'bbb.img'
         }
       ],
-      showUrl: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+      showUrl
     })
   }
 </script>
 <template>
   <div class="demo-image">
     <div class="block">
-      <el-image style="width: 100px; height: 100px" :src="url" fit="fill" @click="toPreview" />
+      <el-image style="width: 100px; height: 100px" :src="url" fit="fill" @click="toPreview(url)" v-for="url in urls" ::key="url"/>
     </div>
   </div>
 </template>
